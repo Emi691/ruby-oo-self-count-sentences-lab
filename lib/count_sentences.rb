aeec3f4e43a389c42ad6099778f80273a5a41d0f
Emiko Nagasawa-Pijoan <enpijoan@gmail.com>
2021-01-20 17:01:17 -0800

    commit tree 2c04267ad2696fd8cc64af9bb546cf54791055d9

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,35 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".") == true
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?") == true
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!") == true
+      return true
+    else
+      return false
+    end
   end
 
   def count_sentences
-
+    if 
+      self.split(/\?|\.|\!/) == []
+      return 0
+    else
+      self.split(/\?|\.|\!/).reject{|string| string.empty?}.count
+    end
   end
 end
